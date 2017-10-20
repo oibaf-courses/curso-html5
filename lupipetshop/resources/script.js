1ddf69a9ae52a9203612eab4421b14ec08f9a214
Fábio Tramasoli <fabio@tramasoli.com>
2017-10-20 09:48:40 -0200

    Aula 8 - JS

---
+++ lupipetshop/resources/script.js
@@ -0,0 +1,12 @@
+/* */
+
+var variaveis = {
+    nome : "Ok",
+    telefone : "456496789",
+    endereco : "Ok dois e depois",
+    dataNascimento : "05/10/1200",
+}
+
+for(item in variaveis) {
+    alert(item+": "+variaveis[item]);
+}
